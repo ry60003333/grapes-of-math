06dbbd955fecdd2e13e49fd26666e1650007dc18
Peter Gyory <ptg3662@rit.edu>
2015-11-29 15:34:44 -0500

    Generate verticies

--- grape.py
+++ grape.py
@@ -1,19 +1,32 @@
 import pygame
 from gi.repository import Gtk
 from entity import Entity
+from math import cos, sin
 
 import entity
 # Grape class for grapes
 class Grape(Entity):
-    def __init__(self, x, y, verts):
+    def __init__(self, x, y, numVerts):
         Entity.__init__(self, x, y)
-        self.r = 10
+        self.r = 20
         self.falling = False
-        self.verts = verts
+        self.verts = self.createVerts(numVerts)
+        print self.verts
 
     def update(self):
         if self.falling == True:
             self.y += 5
+
+    def createVerts(self, numVerts):
+        theta = 360/numVerts
+        verts = []
+        while numVerts > 0:
+            x = self.r * cos(theta * numVerts)
+            y = self.r * sin(theta * numVerts)
+            verts.append((x, y))
+            numVerts -= 1
+
+        return verts
 
     def draw(self, screen):
         # Temp draw function, I'll figure out points soon
